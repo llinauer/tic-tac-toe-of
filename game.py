a6f653e7a91e4fdcd031b910528bb37633f94d0b
llinauer <lukaslinauer1@gmail.com>
2023-01-28 09:53:50 +0100

    Remove computer_vs_computer function from game.py, introduce human_side argument

--- game.py
+++ game.py
@@ -21,11 +21,11 @@ BOARD_COLS = 3
 class Game:
     """Game class"""
 
-    def __init__(self, vs_human=False):
+    def __init__(self, human_side):
         """init method"""
 
-        self.player1 = Player('X')
-        self.player2 = Player('O', vs_human=vs_human)
+        self.player1 = Player('X', human=human_side == 'X')
+        self.player2 = Player('O', human=human_side == 'O')
         self.state = State(self.player1, self.player2)
 
     def player_vs_computer(self):
@@ -61,56 +61,6 @@ class Game:
 
         # start main loop of the window
         self.window.mainloop()
-
-    def computer_vs_computer(self, rounds=100):
-        """Computer plays against itself for rounds(int)
-           Rounds will be given as a comand line argument in tic_tac_toe.py
-           Default is 100"""
-
-        for i in range(rounds):
-
-            while not self.state.is_end:
-
-                # Player 1 turn
-                available_positions = self.state.get_available_positions()
-                p1_action = self.player1.choose_action(available_positions,
-                                                       self.state.board, self.state.player_symbol)
-                self.state.update_state(p1_action)
-                board_hash = get_hash(self.state.board)
-                self.player1.add_state(board_hash)
-
-                # check if the player 1 has won, lost or drawed
-                win_int = self.state.check_win()
-                if win_int:
-                    self.state.give_reward(win_int)
-                    self.player1.reset()
-                    self.player2.reset()
-                    self.state.reset()
-                    break
-
-
-                # Player 2 turn
-                available_positions = self.state.get_available_positions()
-
-                p2_action = self.player2.choose_action(available_positions,
-                                                       self.state.board,
-                                                       self.state.player_symbol)
-                self.state.update_state(p2_action)
-                board_hash = get_hash(self.state.board)
-                self.player2.add_state(board_hash)
-
-                # check if the player 2 has won, lost or drawed
-                win_int = self.state.check_win()
-                if win_int:
-                    self.state.give_reward(win_int)
-                    self.player1.reset()
-                    self.player2.reset()
-                    self.state.reset()
-                    break
-
-        # save the learned values from each player
-        self.player1.save_policy()
-        self.player2.save_policy()
 
     def field_clicked(self, row, col):
         """Gets called when one of the fields is clicked.
