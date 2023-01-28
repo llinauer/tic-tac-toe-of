9b75c8d1fb10b43ddc2ba68315628f11969e9513
llinauer <lukaslinauer1@gmail.com>
2023-01-28 09:48:11 +0100

    Add side cl argument to choose between x and o

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,7 +7,6 @@ Main module, parses command line arguments and sets up game
 
 """
 
-import sys
 import argparse
 from game import Game
 
@@ -15,10 +14,14 @@ from game import Game
 def main():
     """Main function"""
 
-
     # parse arguments
-    parser = argparse.ArgumentParser(description='Train an Reinforcement Learning agent in Tic '
-                                                 'Tac Toe and play against it')
+    parser = argparse.ArgumentParser(description='Play against an old-fashioned tic-tac-toe AI agent')
+    parser.add_argument('-s', '--side', help='Choose side to play (X or O)', required=True)
+
+    args = parser.parse_args()
+    if args.side not in ['x', 'X', 'o', 'O']:
+        print('Please choose x or o')
+        return
 
     # initialize new game
     tic_tac_toe_game = Game(vs_human=True)
