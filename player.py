80dc248078f5dbb4d6b250c1462dca8dd357c9eb
llinauer <lukaslinauer1@gmail.com>
2023-01-28 13:19:02 +0100

    Implement choose_action method

--- player.py
+++ player.py
@@ -10,6 +10,8 @@ Chooses its move based on a simple set of rules.
 BOARD_ROWS = 3
 BOARD_COLS = 3
 
+import numpy as np
+
 
 def get_available_in_row(board, row):
     """ Return positions in board(np.ndarray) of row(int) with no X or O in them"""
@@ -84,6 +86,8 @@ def check_two_in_a_row(board, symbol):
     elif sum([board[i, BOARD_COLS - i - 1] for i in range(BOARD_COLS)]) == -2*symbol:
         return get_available_off_main_diagonals(board)[0], -symbol
 
+    return (None, None), None
+
 
 class Player:
     """Player class"""
@@ -102,8 +106,60 @@ class Player:
            return which action(tuple) should be taken"""
 
         # Rules for perfect play: https://en.wikipedia.org/wiki/Tic-tac-toe#Strategy
+
         # Rule 1: If you have two in a row, place the third to win
+        pos, found_symbol = check_two_in_a_row(current_board, symbol)
 
+        # check if rule 1 applies
+        if pos[0] is not None and found_symbol == symbol:
+            return pos
 
+        # Rule 2: If the opponent has two in a row, block the third to prevent from winning
+        if pos[0] is not None and found_symbol == -symbol:
+            return pos
 
-        return action
+        # Rule 3 & 4: If you have two in opposing corners cause a fork so that you have two lines of winning
+        # If the opponent can fork, prevent it
+        # first, main diagonal
+        pos, found_symbol = get_available_main_diagonals(current_board)
+        if pos[0] is not None and pos[0] == pos[1]:
+            if current_board[[0, 2]] == 0:
+                return 0, 2
+            elif current_board[[2, 0]] == 0:
+                return 2, 0
+
+        # second, off-diagonal
+        pos, found_symbol = get_available_off_main_diagonals(current_board)
+        if pos[0] is not None and pos[0] == pos[1]:
+            if current_board[[0, 0]] == 0:
+                return 0, 0
+            elif current_board[[2, 2]] == 0:
+                return 2, 2
+
+        # Rule 5: If it is the first move, place in some corner
+        if np.sum(current_board) == 0:
+            return tuple(np.random.choice([0, 2], size=2))
+
+        # Rule 6: If the opponent is in a corner, play the opposite corner
+        for corner in [(0, 0), (0, 2), (2, 0), (2, 2)]:
+            if current_board[corner] == -symbol:
+                # return the opposite corner
+                # if 0 -> 2, if 2 -> 0 (this can be achieved by adding 2 and then modulo 4)
+                return (corner[0] + 2) % 4, (corner[1] + 2) % 4
+
+        # Rule 7: If there is an empty corner, place there
+        for corner in [(0, 0), (0, 2), (2, 0), (2, 2)]:
+            if current_board[corner] == 0:
+                return corner
+
+        # Rule 8: If there is an side ( = middle of top/bottom row or left/right col), place there
+        # top & bottom row
+        for i in [0, 2]:
+            if current_board[(i, 1)] == 0:
+                return i, 1
+        # left & right col
+        for j in [0, 2]:
+            if current_board[(1, j)] == 0:
+                return 1, j
+
+        return None, None
